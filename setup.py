5c0763e46ee6a6390ab120cdc79db4dc702d3068
caolan947 <caolan.daly94@gmail.com>
2022-07-29 11:27:10 +0100

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'Hello world.'
+LONG_DESCRIPTION = 'A package that says Hello world! My first pypi package.'
+
+setup(
+    name="hellopkgcdaly",
+    version=VERSION,
+    author="caolan947 (Caolán Daly)",
+    author_email="<caolan.day94@gmail.com>",
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=[],
+    keywords=['python', 'helloworld', 'hello', 'world'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
