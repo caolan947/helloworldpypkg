5610162d89e08562ff69c5bc069d0f140b602fb3
caolan947 <caolan.daly94@gmail.com>
2022-07-29 14:26:25 +0100

    Added unit test

---
+++ libname/test/test_hello.py
@@ -0,0 +1,17 @@
+import unittest
+from unittest.mock import Mock, patch
+
+import hello
+
+class TestSqlService(unittest.TestCase):
+
+    def test_say_hello(self):
+
+        expected_result = "Hello world!"
+        actual_result = hello.say_hello()
+
+        with self.subTest("say_hello() unit test"):
+            self.assertEqual(expected_result, actual_result)        
+
+if __name__ == "__main__":
+    unittest.main()
